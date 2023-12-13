98e445ced9e776aceb729258ca9c222d8b9032b5
Nekgate <Nekgate@gmail.com>
2023-12-13 02:36:04 -0800

    ALX task for stack and queues

---
+++ the_queue.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * f_queue - The prints the top
+ * @head: The stack head
+ * @counter: line number
+ * Return: None
+*/
+
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - This add node to the tail stack
+ * @n: new_value
+ * @head: The stack head
+ * Return: None
+*/
+
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
