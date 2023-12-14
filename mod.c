f51654e517bb096c34042694f5458023e495eca7
Titus Denis <deetitos@gmail.com>
2023-12-14 22:49:19 +0100

    Done

--- mod.c
+++ mod.c
@@ -9,7 +9,7 @@
 void f_mod(stack_t **head, unsigned int counter)
 {
 	stack_t *h = *head;
-	int len = 0, aux;
+	int len = 0, current;
 
 	while (h)
 	{
@@ -18,7 +18,7 @@ void f_mod(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
@@ -33,8 +33,8 @@ void f_mod(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n % h->n;
-	h->next->n = aux;
+	current = h->next->n % h->n;
+	h->next->n = current;
 	*head = h->next;
 	free(h);
 }
